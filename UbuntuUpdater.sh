6ebf513c79c793e4094f5e15f48d24709992098a
David Latorre <david@latorredev.com>
2023-12-28 20:14:00 -0500

    [ADD] add script to automate ubuntu updating

---
+++ UbuntuUpdater.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+# Ubuntu cleaner
+echo "******************"
+echo "* Ubuntu Cleaner *"
+echo "******************"
+sudo apt update -y && \
+sudo apt upgrade -y && \
+sudo flatpak update -y && \
+sudo snap refresh -y && \
+sudo apt autoremove && \
+sudo apt autoclean -y
+echo "*****************************"
+echo "* Ubuntu Cleaned, have fun *" 
+echo "*****************************"
